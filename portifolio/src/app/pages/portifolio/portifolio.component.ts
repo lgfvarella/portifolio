380e0be920300e71d854b5e13e83059eb961d057
Luis <lgfvarella@gmail.com>
2023-06-27 19:07:39 -0300

    antes cardComponent

--- portifolio/src/app/pages/portifolio/portifolio.component.ts
+++ portifolio/src/app/pages/portifolio/portifolio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portifolio',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./portifolio.component.css']
 })
 export class PortifolioComponent {
+
 
 }
